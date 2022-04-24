96d8408fb48a5028f38c3766357a27f7a44d13e3
Marcus Vinicius P C Santos <83514936+Marcus-vinicius-pcs@users.noreply.github.com>
2022-04-24 19:49:27 -0300

    ex-13

---
+++ Lista-de-exercicios-1/Ex-13/ex-13.c
@@ -0,0 +1,58 @@
+/*
+Variação 1: remover as arestas que provocam ciclo.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define true 1;
+#define false 0;
+
+typedef int bool;
+
+typedef struct adj {
+    int v;
+    int flag;
+    int pond;
+    struct adj* prox;
+} NO;
+
+typedef struct {
+    NO* inicio;
+} VERTICE;
+
+bool excluirAresta(VERTICE *g, int i, int j)
+{
+    NO *ant;
+    NO *atual = buscaAresta(g, i, j, &ant);
+    if (!atual)
+        return false;
+    if (ant)
+        ant->prox = atual->prox;
+    else
+        g[i].inicio = atual->prox;
+    free(atual);
+    return true;
+}
+
+void RemoveCiclos(VERTICE* g, int i, bool* ciclo){
+    *ciclo = false;
+    g[i].inicio->flag = 1;
+    NO* p = g[i].inicio->prox;
+    while(p){
+        if(g[p->v].inicio->flag == 1){
+            *ciclo = true;
+            printf("ciclo de %i para %i", i, p->v);
+            if(excluirAresta(g, i, p->v))
+                printf("Aresta excluída\n");
+            else 
+                printf("Erro de exclusão!\n");
+        }
+        if(g[p->v].inicio->flag == 0){
+            ContemCiclo(g, p->v, ciclo);
+        }
+        p = p->prox;
+        g[i].inicio->flag = 2;
+    }
+}

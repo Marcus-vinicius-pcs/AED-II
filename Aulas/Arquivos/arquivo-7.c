9b79f4fd2b5aa68f4da2d189d2e6acef89559cfb
Marcus Vinicius P C Santos <83514936+Marcus-vinicius-pcs@users.noreply.github.com>
2022-06-09 19:26:41 -0300

    m

---
+++ Aulas/Arquivos/arquivo-7.c
@@ -0,0 +1,11 @@
+/*
+Replacement Selection
+(1) Ler chaves do arquivo de entrada até encher p
+(2) Escreve a menor chave de P na saída atual 
+(3) Lê nova chave e compara com a que foi escrita.
+Se maior do que a escrita, insere em p; senão insere em s
+(4) Repete os passos 2 e 3 até esvaziar p
+(5) Fecha corrida atual 
+(6) Inicia nova corrida
+(7) P = S
+*/

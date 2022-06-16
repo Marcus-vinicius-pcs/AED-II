70ddc9ddeceabd40b8a4f55d85a5ca2658bf4922
Marcus Vinicius P C Santos <83514936+Marcus-vinicius-pcs@users.noreply.github.com>
2022-06-16 01:35:49 -0300

    m

--- Aulas/Arquivos/arquivo-5.c
+++ Aulas/Arquivos/arquivo-5.c
@@ -1,36 +1,75 @@
 //KEYSORT
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 20;
 
 typedef struct{
     char chave[20];
     int end;
-} REG;
+} REG_AUX;
 
-REG tabela[MAX];
+REG_AUX tabela[MAX];
 int prox = 0;
-int s = sizeof(REG);
 
-//bool inserir(&tabela, registro, chave, end)
+//bool inserir(&tabela, chave, end)
 //int busca(&tabela, chave)
 //int excluir(&tabela, chave)
 
-REG r;
-FILE* arq = fopen("dados.bin", "rb");
-while(1 == fread(&r, s, 1, arq)){
-    bool ok = inserir(&tabela, r, chave, prox);
-    if(!ok) break;
-    prox++;
+void preencherTabela(REG_AUX* tabela[]){
+    REG_AUX r;
+    FILE* arq = fopen("dados.bin", "rb");
+    while(1 == fread(&r, sizeof(REG_AUX), 1, arq)){
+        if(!inserir(&tabela, r->chave, prox);) 
+            break;
+        prox++;
+    }
+    fclose(arq);
 }
-fclose(arq);
 /*
-1 - Copiar a tarefa de chars e end
+1 - Copiar a tabela de chars e end
 Em memória principal
 
 2 - Percorre a tabela usando um contador
 
-3 - Pata cada item da tabela troca registro eo endereço com registro do novo endereço (contador)
+3 - Para cada item da tabela troca registro e o endereço com registro do novo endereço (contador)
 */
 
-int end = busca(tabela, r, chave);
+void ordenaTabela(REG_AUX* tabela[]){
+    int menorValor = 0;
+    REG_AUX aux = NULL;
+    for(int i = 0; i < MAX; i++){
+        menorValor = i;
+        for(int j = 0; j < MAX; j++){
+            if(tabela[j].chave < tabela[menorValor].chave)
+                menorValor = j;
+        }
+        aux = tabela[menorValor];
+        tabela[menorValor] = tabela[i];
+        tabela[i] = aux;
+    }
+    free(aux);
+}
+
+void ordenarArquivo(FILE* arq){
+    FILE* arq2 = fopen("Arquivo_ordenado.bin", "wb");
+    REG_AUX r;
+    REG_AUX tabela;
+    preencherTabela(&tabela);
+    ordenarTabela(&tabela);
+
+    while(1 == fread(&r, sizeof(REG_AUX), 1, arq)){
+        int end = busca(tabela, r->chave);
+        
+        fseek(arq, end*sizeof(REG_AUX), SEEK_SET);
+        fread(&r, sizeof(REG_AUX), 1, arq);
+        fwrite(&r, sizeof(REG_AUX), 1, arq2);
+    }
+
+    fclose(arq2);
+}
+
+
+int end = busca(tabela, r->chave);
 if(end > -1)
 //erro
 
@@ -42,7 +81,7 @@ if(end == -1)
 //ERRO
 
 fseek(tabela, s*end, SEEK_SET);
-rvazo.valido = 0;
+rvazio.valido = 0;
 fwrite(&rvazio, s, 1, arq);
 
 int end = buscaindice(tabela, chave);

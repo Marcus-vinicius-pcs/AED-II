78cb5b7b4f606152dd89d295956d6a0981613dd6
Marcus Vinicius P C Santos <83514936+Marcus-vinicius-pcs@users.noreply.github.com>
2022-07-04 19:04:10 -0300

    main function

--- EP-2/ep-2.cpp
+++ EP-2/ep-2.cpp
@@ -72,19 +72,23 @@ c1.chave = 0;
 c1.linkdir = 1;
 CHLINK c2;
 c2.chave = 13;
-c2.chave = 2;
+c2.linkdir = 2;
 p1.item[0] = c1;
 p1.item[1] = c2;
+CHLINK c27;
+c27.chave = -1;
+c27.linkdir = -1;
+p1.item[2] = c27;
 
 PAGINA p2;
-p1.np = 1;
-p1.cont = 2;
+p2.np = 1;
+p2.cont = 2;
 CHLINK c3;
 c3.chave = 0;
 c3.linkdir = 3;
 CHLINK c4;
 c4.chave = 4;
-c4.chave = 4;
+c4.linkdir = 4;
 CHLINK c5;
 c5.chave = 8;
 c5.linkdir = 5;
@@ -93,14 +97,14 @@ p2.item[1] = c4;
 p2.item[2] = c5;
 
 PAGINA p3;
-p1.np = 2;
-p1.cont = 2;
+p3.np = 2;
+p3.cont = 2;
 CHLINK c6;
 c6.chave = 0;
 c6.linkdir = 6;
 CHLINK c7;
 c7.chave = 17;
-c7.chave = 7;
+c7.linkdir = 7;
 CHLINK c8;
 c8.chave = 21;
 c8.linkdir = 8;
@@ -109,14 +113,14 @@ p3.item[1] = c7;
 p3.item[2] = c8;
 
 PAGINA p4;
-p1.np = 3;
-p1.cont = 2;
+p4.np = 3;
+p4.cont = 2;
 CHLINK c9;
 c9.chave = 0;
 c9.linkdir = -1;
 CHLINK c10;
 c10.chave = 1;
-c10.chave = -1;
+c10.linkdir = -1;
 CHLINK c11;
 c11.chave = 3;
 c11.linkdir = -1;
@@ -125,14 +129,14 @@ p4.item[1] = c10;
 p4.item[2] = c11;
 
 PAGINA p5;
-p1.np = 4;
-p1.cont = 2;
+p5.np = 4;
+p5.cont = 2;
 CHLINK c12;
 c12.chave = 0;
 c12.linkdir = -1;
 CHLINK c13;
 c13.chave = 5;
-c13.chave = -1;
+c13.linkdir = -1;
 CHLINK c14;
 c14.chave = 6;
 c14.linkdir = -1;
@@ -141,14 +145,14 @@ p5.item[1] = c13;
 p5.item[2] = c14;
 
 PAGINA p6;
-p1.np = 5;
-p1.cont = 2;
+p6.np = 5;
+p6.cont = 2;
 CHLINK c15;
 c15.chave = 0;
 c15.linkdir = -1;
 CHLINK c16;
 c16.chave = 9;
-c16.chave = -1;
+c16.linkdir = -1;
 CHLINK c17;
 c17.chave = 10;
 c17.linkdir = -1;
@@ -157,14 +161,14 @@ p6.item[1] = c16;
 p6.item[2] = c17;
 
 PAGINA p7;
-p1.np = 6;
-p1.cont = 2;
+p7.np = 6;
+p7.cont = 2;
 CHLINK c18;
 c18.chave = 0;
 c18.linkdir = -1;
 CHLINK c19;
 c19.chave = 15;
-c19.chave = -1;
+c19.linkdir = -1;
 CHLINK c20;
 c20.chave = 26;
 c20.linkdir = -1;
@@ -173,14 +177,14 @@ p7.item[1] = c19;
 p7.item[2] = c20;
 
 PAGINA p8;
-p1.np = 7;
-p1.cont = 2;
+p8.np = 7;
+p8.cont = 2;
 CHLINK c21;
 c21.chave = 0;
 c21.linkdir = -1;
 CHLINK c22;
 c22.chave = 18;
-c22.chave = -1;
+c22.linkdir = -1;
 CHLINK c23;
 c23.chave = 19;
 c23.linkdir = -1;
@@ -189,14 +193,14 @@ p8.item[1] = c22;
 p8.item[2] = c23;
 
 PAGINA p9;
-p1.np = 8;
-p1.cont = 2;
+p9.np = 8;
+p9.cont = 2;
 CHLINK c24;
 c24.chave = 0;
 c24.linkdir = -1;
 CHLINK c25;
 c25.chave = 23;
-c25.chave = -1;
+c25.linkdir = -1;
 CHLINK c26;
 c26.chave = 26;
 c26.linkdir = -1;
@@ -218,11 +222,19 @@ PAGINA p;
 rewind(arq);
 while(1 == fread(&p, sizeof(PAGINA), 1, arq))
 {
-    printf("Página: %i\n\n", p.np);    
-    for(int i = 0; i < 2; i++){
-        if(p.item[i].chave != 0)
-            printf("Chave: %i Linkdir: %i\n", p.item[i].chave, p.item[i].linkdir);
+    printf("Página: %i\n\n", p.np); 
+  
+    for(int i = 0; i < 3; i++){
+      if(p.item[i].chave != -1)
+      printf("Chave: %i Link da chave %i\n", p.item[i].chave, p.item[i].linkdir);
     }
+    printf("Links:\n");
+    for(int j = 0; j < 3; j++){
+      if(p.item[j].linkdir)
+        printf(" %i ", p.item[j].linkdir);
+        
+    }
+  printf("\n\n");
 }
   fclose(arq);
 }
